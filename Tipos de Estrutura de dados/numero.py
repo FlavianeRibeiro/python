44628811f3027708e5dcd09109e428ec4ac2a77e
Flaviane Ribeiro <rnany13@gmail.com>
2018-02-08 20:24:52 +0000

    aula 01

---
+++ Tipos de Estrutura de dados/numero.py
@@ -0,0 +1,47 @@
+#python possui 2 tipos de números principais
+#int  - Possui precisão limitada
+#Float - 53 bits de precoção e 15 a 16 de precisão decimal
+
+#soma
+4 + 4
+
+# Subtração
+4 - 3
+
+# Multiplicação 
+2*2
+
+# Divisão
+3 / 2
+
+# Resultado da divisão é inteiro
+4 // 3
+
+# Potência
+4 ** 2
+
+#Módulo
+10 % 3
+
+#Type - mostra o tipo do objeto
+type(5)
+
+#Conversão 
+
+# float
+float(9)
+
+#inteiro - redonda para baixo
+int(5.5)
+
+
+# Funções abs, round e pow
+# abs - retorna o valor absoluto
+abs(8)
+
+#round - retorna i valor com arredondamento
+round(3.145682, 2)
+
+#Potência 
+pow(4,2)
+
---
+++ Tipos de Estrutura de dados/variavel_e_operadores.py
@@ -0,0 +1,6 @@
+#imprindo valor
+print(10)
+
+a = 'isso é um teste'
+
+print(a)
